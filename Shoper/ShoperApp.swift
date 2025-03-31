db50f08dff39f1c46dcd185c2e2846aa425b83c1
Mekhak <mekhakghapantsyan@simplytechnologies.com>
2025-03-31 11:40:16 +0400

    Initial Commit

---
+++ Shoper/ShoperApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoperApp.swift
+//  Shoper
+//
+//  Created by Mekhak Ghapantsyan on 3/31/25.
+//
+
+import SwiftUI
+
+@main
+struct ShoperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
